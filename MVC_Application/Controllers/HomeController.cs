82efa401ca5894f6904fea1ec18147a9dc4affae
moaz-ze69 <98176958+moaz-ze69@users.noreply.github.com>
2023-02-14 15:12:20 +0500

    initial commit

---
+++ MVC_Application/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(HomeModel home)
+        {
+            int lower = home.LowerBound;
+            int upper = home.UpperBound;
+
+            ViewBag.Lower = lower;
+            ViewBag.Upper = upper;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_Application/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Application.Models
+{
+    public class HomeModel
+    {
+        public int LowerBound { get; set; }
+        public int UpperBound { get; set; }
+    }
+}
